f59e853720d47b321476adff693131a06c9560ab
Anton <anton_polisko@gmail.com>
2026-01-04 20:44:36 +0100

    casino

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import AntonCase from "./pages/AntonCase";
+import NilSlot from "./pages/NilSlot";
+import ComingSoon from "./pages/ComingSoon";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +22,10 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio/anton-case" element={<AntonCase />} />
+          <Route path="/portfolio/nil-slot" element={<NilSlot />} />
+          <Route path="/portfolio/coming-soon" element={<ComingSoon />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
